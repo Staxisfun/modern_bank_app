dad462c019520c374e9e904c5e9a570f44a54d9d
Matt <Mwilson82@hotmail.com>
2023-01-18 15:44:30 -0800

    fix typo errors in app.jsx and complete navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxwidth}`}>
+      <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.flexstart}`}>
+    <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexstart}`}>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
