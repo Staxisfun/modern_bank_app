ba42181698c21d817b252ba67de67f02441d4e1c
Matt <Mwilson82@hotmail.com>
2023-02-09 20:28:54 -0800

    complete testimonials and client sections

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -8,13 +8,13 @@ const CardDeal = () => (
       <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden' /> in few easy steps.</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
       </p>
+
       <Button styles="mt-10" />
     </div>
 
-    <div className='layout.sectionImg'>
-      <img src={card} alt="card" className='w-[100%] h-{100%]' />
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className='w-[100%] h-[100%]' />
     </div>
-
   </section >
 );
 
